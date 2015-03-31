176bd28bbe61f9e1afb2c127464a03f4e656e0ad
Dan Spitz <spitz.dan.l@gmail.com>
2015-03-31 16:08:18 -0400

    fix bugs in test data generation code

--- scripts/generate_test_data.rb
+++ scripts/generate_test_data.rb
@@ -1,4 +1,5 @@
-require 'date'
+require 'mongo'
+include Mongo
 
 def setup_db_indexes
 	db = MongoClient.new.db('test')
@@ -8,26 +9,31 @@ end
 def generate_test_data
 	db = MongoClient.new.db('test')
 	[1,2,3,4,5].each do |practice_ext_id|
-		[0...1000].to_a.each do |appointment_ext_id|
-			apt = db['appointments'].find_and_modify(
-				query: {
-					'practice_external_id' => practice_ext_id,
-					'appointment_external_id' => appointment_ext_id
-				},
-				upsert: true,
-				:new => true
-			)
+		(0...1000).each do |appointment_ext_id|
+			db['appointments'].insert({
+				'practice_external_id' => practice_ext_id,
+				'appointment_external_id' => appointment_ext_id
+			})
 
 			if appointment_ext_id < 700
+				apt = db['appointments'].find_one({
+					'practice_external_id' => practice_ext_id,
+					'appointment_external_id' => appointment_ext_id
+				})
+
 				prob = rand()
 				prediction = prob > 0.5
-				date = Date.today
+				date = Time.now.utc
 				db['predictions'].insert({
 					'appointment_id' => apt['_id'],
 					'prediction' => prediction,
 					'probability' => prob,
 					'date' => date
 				})
+			end
 		end
 	end
 end
+
+setup_db_indexes
+generate_test_data
